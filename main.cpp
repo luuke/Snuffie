437daebd7b09ed36c94ca2c75bb305cd9afa7d3d
Luuke <lukasz.bartosz1990@gmail.com>
2012-05-27 09:08:08 +0200

    git test

--- main.cpp
+++ main.cpp
@@ -5,6 +5,22 @@
  *      Author: Luke
  */
 
+//#include <avr/io.h>
+#include "snuffie.h"
 
+int main(){
+	snuffie autko;
+	//Snuffie.init();
+	SBI(DDRD,7);
+	while(1){
+		//Snuffie.led_on();
+		SBI(PORTD,7);
+		_delay_ms(400);
+		//Snuffie.led_off();
+		CBI(PORTD,7);
+		_delay_ms(400);
+	}
+	return 0;
+}
 
 
